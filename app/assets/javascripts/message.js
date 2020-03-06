482676fde0328d32f81101dbeca5d0525ee2d391
kawaguchihitoshi <61231995+kawaguchihitoshi@users.noreply.github.com>
2020-03-06 19:41:53 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -79,7 +79,6 @@ $(function(){
   } else if (message.image) {
     var html = 
     `<div class="message"data-message-id= ${message.id}>
-    ${message.id}
       <div class="upper-message">
         <div class="upper-message__user-name">
           ${message.user_name}
