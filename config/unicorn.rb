bcd349d3beb459096eae6f7d08d835e15f9c5b1e
kawaguchihitoshi <61231995+kawaguchihitoshi@users.noreply.github.com>
2020-03-06 16:48:09 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -26,11 +26,7 @@ before_fork do |server, worker|
   defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.connection.disconnect!
 
-<<<<<<< HEAD
   if run_once
-=======
-  if run_once 
->>>>>>> master
     run_once = false # prevent from firing again
   end
 
